7d92efb5b4ddeadde77a8e98a6b207002899c5b7
Emma de Jong <00078079@uwa.edu.au>
2023-09-13 16:09:20 +0800

    code for Supp. Table 1

---
+++ code/1-ncbi-full-table.R
@@ -0,0 +1,132 @@
+#-----------------------------------------------------------------------------------------------------------------
+# Code to generate Supplementary Table 1
+#-----------------------------------------------------------------------------------------------------------------
+
+
+#### Load libraries ----------------------------------------------------------------------------------------------
+
+library(here)         # simplify file paths
+library(tidyverse)    # data wrangling+
+library(readxl)       # read excel files
+
+
+#### Read in data ----------------------------------------------------------------------------------------------
+
+w <- read.csv(here("data/marine-species.csv"))
+df <- read_xlsx(here("data/Chordata.xlsx"))
+iucn <- read.csv(here("data/Table4a_IUCNRedList.csv"))
+
+
+#### Data wrangling ----------------------------------------------------------------------------------------------
+
+## Remove non-marine assemblies from NCBI data - match on either accepted name or scientific name
+mv <- df |> filter(`Organism Name` %in% w$acceptedNameUsage | `Organism Name` %in% w$scientificName)
+
+## Add taxonomic data 
+
+# genus
+mv <- mv |> mutate(genus = ifelse(`Organism Name` %in% w$`acceptedNameUsage`, w$genus[match(`Organism Name`, w$`acceptedNameUsage`)],
+                                  ifelse(`Organism Name` %in% w$`scientificName`, w$genus[match(`Organism Name`, w$`scientificName`)],
+                                         NA)))
+
+# family
+mv <- mv |> mutate(family = ifelse(`Organism Name` %in% w$`acceptedNameUsage`, w$family[match(`Organism Name`, w$`acceptedNameUsage`)],
+                                   ifelse(`Organism Name` %in% w$`scientificName`, w$family[match(`Organism Name`, w$`scientificName`)],
+                                          NA)))
+
+# order
+mv <- mv |> mutate(order = ifelse(`Organism Name` %in% w$`acceptedNameUsage`, w$order[match(`Organism Name`, w$`acceptedNameUsage`)],
+                                  ifelse(`Organism Name` %in% w$`scientificName`, w$order[match(`Organism Name`, w$`scientificName`)],
+                                         NA)))
+
+# class
+mv <- mv |> mutate(class = ifelse(`Organism Name` %in% w$`acceptedNameUsage`, w$class[match(`Organism Name`, w$`acceptedNameUsage`)],
+                                  ifelse(`Organism Name` %in% w$`scientificName`, w$class[match(`Organism Name`, w$`scientificName`)],
+                                         NA)))
+
+## Re-order the columns
+mv <- mv %>%
+  relocate(genus, family, order, class, .after = `Organism Common Name`)
+
+## Remove invertebrates
+mv <- mv |> filter(!class %in% c("Appendicularia","Ascidiacea","Leptocardii","Thaliacea"))
+wv <- w |> filter(!class %in% c("Appendicularia","Ascidiacea","Leptocardii","Thaliacea"))
+
+## Retain "accepted" names only to obtain full list of marine species
+wv.species <- wv |> filter(taxonomicStatus == "accepted")
+
+## Check for duplicates 
+table(duplicated(mv$`Assembly Accession`))
+table(duplicated(wv.species$acceptedNameUsage))
+
+## Remove the duplicates (not removing the duplicate from the full list of species)
+mv <- mv |> filter(!duplicated(mv$`Assembly Accession`))
+
+## Format data types for IUCN data frame
+iucn$Subtotal..threatened.spp.. <- as.numeric(gsub(",", "", iucn$Subtotal..threatened.spp..))
+iucn$Total <- as.numeric(gsub(",", "", iucn$Total))
+
+
+#### Manual curation ----------------------------------------------------------------------------------------------
+
+## Remove species that have incorrectly been labelled as marine - see `docs/1-ncbi-data.md` for full explanation
+mv <- mv |> filter(!`Organism Name` %in% c("Accipiter virgatus","Aythya fuligula","Coregonus clupeaformis","Hypophthalmichthys nobilis","Inia geoffrensis","Squalius cephalus"))
+wv.species <- wv.species |> filter(!scientificName == "Hymenops perspicillatus")
+
+
+#### Supplementary Table 1 ----------------------------------------------------------------------------------------------
+
+## Reduce number of columns - keep those of interest
+ST1 <- mv |> select("Assembly Accession",
+                    "Organism Name",
+                    "Organism Common Name",
+                    "Organism Taxonomic ID",
+                    "class",
+                    "order",
+                    "family",
+                    "genus",
+                    "Assembly Release Date",
+                    "Assembly Sequencing Tech",
+                    "Assembly Stats Contig N50")
+
+## Print the table
+ST1 |> print(n=700)
+
+#### Export data ----------------------------------------------------------------------------------------------
+
+## Export the cleaned data - used for Table 1, and Figures 1 and 2
+write.csv(mv, here("data/marine-vert-refg-ncbi.csv"), row.names = F)
+write.csv(wv.species, here("results/marine-vert-species.csv"), row.names = F)
+
+
+
+## Summarise the number of marine species per order
+all.orders <- wv.species |> group_by(class, order) |> tally() |> arrange(desc(n))
+
+## Remove duplicate species (9 species have two assemblies)
+ncbi.orders <- mv |> filter(!duplicated(`Organism Name`))
+ncbi.orders <- mv |> group_by(class,order) |> tally() |> arrange(desc(n))
+
+## Add the number of ref genomes available for each order
+all.orders$refgenomes <- ncbi.orders$n[match(all.orders$order,ncbi.orders$order)]
+
+## Replace NAs with zeros
+all.orders[which(is.na(all.orders$refgenomes)),"refgenomes"] <- 0
+
+## Add a column that calculates % representation of each order
+all.orders <- all.orders |> mutate(Percentage = round((refgenomes / n) * 100, 1))
+
+## Calculate % threatened
+iucn$PT <- round((iucn$Subtotal..threatened.spp..  / iucn$Total)*100,2)
+
+## Add IUCN data to the table
+all.orders$PT <- iucn$PT[match(toupper(all.orders$order), iucn$Name)]
+all.orders$TT <- iucn$Subtotal..threatened.spp..[match(toupper(all.orders$order), iucn$Name)] # total threatened
+
+## Print the table
+all.orders |> arrange(class,order) |> print(n=200)
+
+
+
+
+
