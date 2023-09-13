2513584df0263dc8a71faa7677dfd82ce4685450
Emma de Jong <00078079@uwa.edu.au>
2023-09-13 16:22:28 +0800

    removed code relating to Table 1

--- code/1-ncbi-full-table.R
+++ code/1-ncbi-full-table.R
@@ -62,10 +62,6 @@ table(duplicated(wv.species$acceptedNameUsage))
 ## Remove the duplicates (not removing the duplicate from the full list of species)
 mv <- mv |> filter(!duplicated(mv$`Assembly Accession`))
 
-## Format data types for IUCN data frame
-iucn$Subtotal..threatened.spp.. <- as.numeric(gsub(",", "", iucn$Subtotal..threatened.spp..))
-iucn$Total <- as.numeric(gsub(",", "", iucn$Total))
-
 
 #### Manual curation ----------------------------------------------------------------------------------------------
 
@@ -95,38 +91,7 @@ ST1 |> print(n=700)
 #### Export data ----------------------------------------------------------------------------------------------
 
 ## Export the cleaned data - used for Table 1, and Figures 1 and 2
-write.csv(mv, here("data/marine-vert-refg-ncbi.csv"), row.names = F)
+write.csv(ST1, here("data/supp-table-1.csv"), row.names = F)
 write.csv(wv.species, here("results/marine-vert-species.csv"), row.names = F)
-
-
-
-## Summarise the number of marine species per order
-all.orders <- wv.species |> group_by(class, order) |> tally() |> arrange(desc(n))
-
-## Remove duplicate species (9 species have two assemblies)
-ncbi.orders <- mv |> filter(!duplicated(`Organism Name`))
-ncbi.orders <- mv |> group_by(class,order) |> tally() |> arrange(desc(n))
-
-## Add the number of ref genomes available for each order
-all.orders$refgenomes <- ncbi.orders$n[match(all.orders$order,ncbi.orders$order)]
-
-## Replace NAs with zeros
-all.orders[which(is.na(all.orders$refgenomes)),"refgenomes"] <- 0
-
-## Add a column that calculates % representation of each order
-all.orders <- all.orders |> mutate(Percentage = round((refgenomes / n) * 100, 1))
-
-## Calculate % threatened
-iucn$PT <- round((iucn$Subtotal..threatened.spp..  / iucn$Total)*100,2)
-
-## Add IUCN data to the table
-all.orders$PT <- iucn$PT[match(toupper(all.orders$order), iucn$Name)]
-all.orders$TT <- iucn$Subtotal..threatened.spp..[match(toupper(all.orders$order), iucn$Name)] # total threatened
-
-## Print the table
-all.orders |> arrange(class,order) |> print(n=200)
-
-
-
 
 
